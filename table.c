138987579571c9216ca2f402339774634abfa03b
sharanya <84262011+Sharanya183@users.noreply.github.com>
2024-09-18 20:25:49 +0530

    Add files via upload

---
+++ table.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    
+    int n, i, prod;
+
+    
+    printf("Enter a positive integer: ");
+    scanf("%d", &n);
+    
+    
+    for(i = 1; i<=10; i++)
+    {
+        prod = n * i;
+        printf("%d * %d = %d\n", n, i, prod);
+    }
+    
+    return 0;
+}
